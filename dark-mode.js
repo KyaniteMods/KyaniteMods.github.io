d26d190d0823c82450b6556b681af9f886ffaa80
Pedro270707 <pedro2007.prrr@gmail.com>
2022-08-30 18:50:16 -0300

    Fixed light mode

--- dark-mode.js
+++ dark-mode.js
@@ -5,7 +5,7 @@ var lightModeButton = document.getElementById("light-mode-button");
 if (localStorage.lightmode) {
 	if (localStorage.lightmode == 'true') {
 		$(wrapper).addClass("light-mode");
-		document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('assets/white_concrete_64x.png')";
+		document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('assets/white_concrete.png')";
 		mcTip.innerHTML = "Switch to Dark Mode";
 		$(lightModeButton).attr("data-mctitle", "Switch to Dark Mode");
 	}
@@ -17,14 +17,14 @@ function toggleDarkMode() {
 	if (localStorage.lightmode == 'true') {
 		console.log("Removing Light Mode");
 		$(wrapper).removeClass("light-mode");
-		document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('assets/deepslate_tiles_background_64x.png')";
+		document.body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('assets/background-blocks/dark-mode/deepslate_tiles.png')";
 		mcTip.innerHTML = "Switch to Light Mode";
 		$(lightModeButton).attr("data-mctitle", "Switch to Light Mode");
 		localStorage.lightmode = 'false';
 	} else {
 		console.log("Adding Light Mode");
 		$(wrapper).addClass("light-mode");
-		document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('assets/white_concrete_64x.png')";
+		document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('assets/background-blocks/light-mode/white_concrete.png')";
 		mcTip.innerHTML = "Switch to Dark Mode";
 		$(lightModeButton).attr("data-mctitle", "Switch to Dark Mode");
 		localStorage.lightmode = 'true';
